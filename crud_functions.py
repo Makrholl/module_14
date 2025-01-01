b83bb739d4d2711a322ea5b1af21b4a3c8b77efc
Makrholl <65885317+Makrholl@users.noreply.github.com>
2025-01-01 18:01:41 +1000

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -1,11 +1,9 @@
 import sqlite3
 
-# Инициализация базы данных
 def initiate_db(db_path="database.db"):
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
 
-    # Создаем таблицу Products, если она еще не создана
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS Products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,12 +15,10 @@ def initiate_db(db_path="database.db"):
     connection.commit()
     connection.close()
 
-# Получить все продукты
 def get_all_products(db_path="database.db"):
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
 
-    # Получаем все записи из таблицы Products
     cursor.execute('SELECT id, title, description, price FROM Products')
     products = cursor.fetchall()
 
